e4e144d45e7dfeb95450a09ee88082d9b3a0e283
Shindi Purnama Putri <50320881+shindipurnama@users.noreply.github.com>
2023-06-06 17:56:25 +0700

    Update pembayaran.blade.php

--- resources/views/pembayaran.blade.php
+++ resources/views/pembayaran.blade.php
@@ -12,7 +12,7 @@
         <div class="col-12">
             <div class="card mb-4">
                 <div class="card-header pb-0">
-                    <h6>Data Pembayaran</h6>
+                    <h6>Data Pembayaran tes tes</h6>
                 </div>
                 <div class="card-body px-0 pt-0 pb-2">
                     <div class="table-responsive">
